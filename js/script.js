51a6b7acbb6a5fbece30cd43924af3a6591f3e3a
Paul Aron Lopez Ortega <plopez@joffroy.com>
2022-12-29 15:31:46 -0700

    Se corrigio scroll

--- js/script.js
+++ js/script.js
@@ -2,20 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const elements = [...Array(10000).keys()];
     const rowHeight = 30;
     const containerHeight = 300;
-    const totalItems = containerHeight / rowHeight;
+    const totalItems = Math.round(containerHeight / rowHeight) + 1;
     const containerElement = document.getElementById('container');
     const viewportHeight = rowHeight * elements.length;
     const viewportElement = document.getElementById('viewport');
     const rowsElement = document.getElementById('rows');
-
+    
     viewportElement.style.height = `${viewportHeight}px`;
     containerElement.style.height = `${containerHeight}px`;
 
     renderNodes(0);
 
     containerElement.addEventListener('scroll', e => {
-        e.preventDefault();
         const start = Math.floor(e.target.scrollTop / rowHeight);
+        const offSetY = start * rowHeight;
+        rowsElement.style.transform = `translateY(${offSetY}px)`;
         renderNodes(start);
     });
 
