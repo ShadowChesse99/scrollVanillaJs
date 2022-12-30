92a743015e138d30e1abc2256bb19499607c2b34
Paul Aron Lopez Ortega <plopez@joffroy.com>
2022-12-30 09:19:54 -0700

    a

--- js/script.js
+++ js/script.js
@@ -1,45 +1,70 @@
 document.addEventListener('DOMContentLoaded', async function () {
     let elements = [];
     let countPages = 1;
-    let isResize = false;
+    let isBusy = false;
 
     const rowHeight = 54;
-    const containerHeight = 300;
-    const totalItems = Math.floor(containerHeight / rowHeight) + 2;
+    const containerHeight = 540;
+    const totalItems = Math.floor(containerHeight / rowHeight) + 3;
     const containerElement = document.getElementById('container');
     const viewportElement = document.getElementById('viewport');
     const tbody = document.getElementById('tbody');
 
     containerElement.style.height = `${containerHeight}px`;
-    await loadData(countPages);
-    renderBody(0);
+    loadData(countPages).then(() => renderBody(countPages))
 
     containerElement.addEventListener('scroll', async function (e) {
         const offsetHeight = e.target.offsetHeight;
         const scrollTop = e.target.scrollTop;
         const scrollHeight = e.target.scrollHeight;
-        if (offsetHeight + scrollTop + 1 >= scrollHeight) {
+        if (!isBusy && offsetHeight + scrollTop + 1 >= scrollHeight) {
             countPages++;
-            await loadData(countPages);
+            await loadData(countPages).then(() => {
+                debugger;
+                const array = [];
+                for (let i = totalItems; i > 0; i--) {
+                    let item = elements[i];
+                    array.push(`
+                    <tr>
+                    <td><img src="${item.picture.thumbnail}"></td>
+                    <td>${item.name.first} ${item.name.last}</td>
+                    <td>${item.gender}</td>
+                    <td>${item.registered.age}</td>
+                    <td>${item.phone}</td>
+                    <td>${item.cell}</td>
+                    </tr>`)
+                }
+                tbody.innerHTML = array.join('');
+            });
+            // .then(() => {
+            //     const start = Math.floor(e.target.scrollTop / rowHeight);
+            //     const offSetY = start * rowHeight;
+            //     return { start, offSetY };
+            // })
+            // .then(({ start, offSetY }) => {
+            //     renderBody(start);
+            //     return offSetY;
+            // })
+            // .then(offSetY => viewportElement.style.transform = `translateY(${offSetY}px)`);
         }
-        else {
-            //Esto es la esta cagango
-            // const start = Math.floor(e.target.scrollTop / rowHeight);
-            // const offSetY = start * rowHeight;
-            // viewportElement.style.transform = `translateY(${offSetY}px)`;
-            // renderBody(start);
-        }
+
+        // const start = Math.floor(e.target.scrollTop / rowHeight);
+        // const offSetY = start * rowHeight;
+        // viewportElement.style.transform = `translateY(${offSetY}px)`;
     });
 
     async function loadData(page) {
+        isBusy = true;
         await fetch(`https://randomuser.me/api/?page=${page}&results=${totalItems}&seed=a8eb78cd70329cc7&inc=name,email,gender,registered,phone,cell,picture,id`)
             .then(response => response.json())
             .then(data => data.results)
             .then(results => {
                 elements = elements.concat(results);
-                const viewportHeight = rowHeight * elements.length + 20;
+                const viewportHeight = rowHeight * elements.length;
                 viewportElement.style.height = `${viewportHeight}px`;
-            });
+            })
+            .then(() => isBusy = false)
+            .catch(() => isBusy = false);
     }
 
     function renderBody(start) {
